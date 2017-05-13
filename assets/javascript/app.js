22f3f2c4dda505eca02b039cb88f28bab126e144
Raquel Rodriguez <raquelclare@Raquels-MacBook-Air.local>
2017-05-13 18:50:49 -0500

    Cleaning up code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -56,38 +56,33 @@ var questions = [
 console.log('hello');
 
 // Creating timer
+// Variable for the interval
 var intervalId;
 
 // Could I create a timer that shows the time in minutes and seconds?
 // var timer = {
-
-// 	time: 120
+// 		time: 120
 // 	// Start of the timer should be 2 minutes and count down.
 // 	// $("#timer").prepend("02:00");
 // };
 
+// Time set to 120 seconds
 var time = 10;
-// var timer = setTimeout(function() {
-
-// });
-
+// Function run that starts my timer after 1 second after the start button is clicked (see below)
 function run() {
+	// Setting the interval to the decrement function after 1 second
 	intervalId = setInterval(decrement, 1000);
 };
-
+// Function decrement to decrease my second left (time) by 1 every second
 function decrement() {
 	time--;
+	// Adds the HTML into timer div
 	$("#timer").html("<h3>Seconds remaining:<br>" + time + "</h3>");
-	if (time === 0){
+	// When time is equal to zero--> alert and go to the stats page
+	if (time === 0) {
 		alert("Times up!")
-		$("#game").hide();
-		$("#submit").hide();
-		$("#timer").hide();
-		// I could definitely could consolidate all of this code if I wanted to
-		$("#stats").append("<h2>All Done!</h2");
-		$("#stats").append("<h3>" + "Correct Answers: " + correctAnswers + "</h3");
-		$("#stats").append("<h3>" + "Incorrect Answers: " + incorrectAnswers + "</h3");
-		$("#stats").append("<h3>" + "Unanswered: " + unanswered + "</h3");
+		// Calls the renderStats function (below)
+		renderStats();
 	}
 };
 
@@ -102,15 +97,29 @@ $("#start").click(function() {
 			// Looping through each object in the questions array to display the value at the 'choices' key
 			for (j = 0; j < questions[i].choices.length; j++){
 				// Appending the values at the choices key and applying a radio button
-				$("#game").append("<input type='radio' name='" + questions[i].answer + "' value='" + questions[i].choices[j] + "'>" 
+				// var value = questions[i].choices[j];
+				$("#game").append("<input type='radio' name='" + questions[i].answer + "' id='" + questions[i].choices[j] + "'>" 
 					+ questions[i].choices[j] + "<br>");
-		}
+				// var choice = $('input[name="' + questions[i].choices[j] + '"]:checked').val();
+				// console.log(choice);
+			}
 	}
 	// Run the timer when the start button is clicked
 	run();
 	// Dynamically adding a 'submit' button into the submit div, to be displayed after the questions
 	$("#submit").append("<button id='submit-button' class='btn btn-success' type='button'>Submit!</button>");
 });
+
+
+//console.log(choice);
+
+// var choice = $("#'" + questions[i].choices[j] + "'").on("click", function() {
+// 	console.log(questions[i].choices[j]);
+// })
+//getElementById(value(questions[i].choices[j]));
+
+// var choice = $('input[name="' + questions[i].answer + '"]:checked').val();
+// console.log(choice);
 
 // Variables to compare correct/incorrect/unaswered questions and answers
 var correctAnswers = 0;
@@ -119,26 +128,40 @@ var unanswered = 10;
 
 // // jQuery to stop game when we click the "submit" button.
 $("#submit").click(function() {
-	$("#game").hide();
-	$("#submit").hide();
-	stop();
-	$("#timer").hide();
-	$("#stats").append("<h2>All Done!</h2");
-	$("#stats").append("<h3>" + "Correct Answers: " + correctAnswers + "</h3");
-	$("#stats").append("<h3>" + "Incorrect Answers: " + incorrectAnswers + "</h3");
-	$("#stats").append("<h3>" + "Unanswered: " + unanswered + "</h3");
-
-
+	// Stops the timer so that you no longer receive an alert and it doesnt append another stats div
+	clearInterval(intervalId);
+	// Calls the renderStats function to take you to the stats page
+	renderStats();
 	console.log("Is this working?");
+
 });
 
 // Stats page should display after timer is done or submit button is clicked
 // Need to compare choice selcted against correct answer and update the stats accordingly
-var choice = getElementById(value(questions[i].choices[j]));
+function answer() {
+	var choice = $("input").on( "click", function() {
+ 		// $('input[name="' + questions[i].choices[j] + '"]:checked').val();
+				console.log(choice);
 if (choice === questions[i].answer){
 	correctAnswers++;
 	unanswered--;
 }else {
 	incorrectAnswers++;
 	unanswered--;
-}
+	}
+})
+};
+
+// Takes you to the Stats page when renderStats is called (submit button or after timer runs out)
+function renderStats() {
+	// Hiding the game, submit and timer divs
+	$("#game").hide();
+	$("#submit").hide();
+	$("#timer").hide();
+	// Appending the HTML needed to display results
+	$("#stats").append("<h2>All Done!</h2");
+	$("#stats").append("<h3>" + "Correct Answers: " + correctAnswers + "</h3");
+	$("#stats").append("<h3>" + "Incorrect Answers: " + incorrectAnswers + "</h3");
+	$("#stats").append("<h3>" + "Unanswered: " + unanswered + "</h3");
+};
+
